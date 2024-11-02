97a921c99f8d23b82feeefd6ca554fd5b7516a7a
Josh Leslie <30437767+JoshMLeslie@users.noreply.github.com>
2024-11-02 13:12:48 -0400

    fix major cities cluster group generation.
    migrate usa pop cities json reference

--- assets/js/mapping/major-city-markers.js
+++ assets/js/mapping/major-city-markers.js
@@ -1,10 +1,9 @@
-
 /* global L:readonly */
 
-import { fetchJSON } from "../util/index.js";
-import { drawMarker } from "./draw.js";
+import { fetchJSON } from '../util/index.js';
+import { drawMarker } from './draw.js';
 
-const showCityMarkerPos = (map, lat, lon, city) => e => {
+const showCityMarkerPos = (map, lat, lon, city) => (e) => {
 	const {lat: layerLat, lng: layerLon} = map.layerPointToLatLng(
 		L.point(e.layerPoint)
 	);
@@ -20,18 +19,21 @@ const showCityMarkerPos = (map, lat, lon, city) => e => {
 	navigator.clipboard.writeText(txt);
 };
 
-export default async function genMajorCityMarkers (map) {
+export default async function genMajorCityMarkers(map) {
 	/** @type {{lat: number; lon: number; city: string;}[]}} data */
-	const data = await fetchJSON('./data-csv/parsed_results.json');
+	const data = await fetchJSON(
+		'./assets/js/const/usa-most-pop-cities-2020.json'
+	);
 	if (!data || !data.length) {
-		throw ReferenceError("missing CSV data");
+		throw ReferenceError('missing CSV data');
 	}
-	const markers = data.map(({lat, lon, city}) => {
+	const clusterGroup = L.markerClusterGroup();
+	data.forEach(({lat, lon, city}) => {
 		const m = drawMarker(map, [lat, lon], city, {color: 'red'});
 		m.on('click', showCityMarkerPos(map, lat, lon, city));
-		return m;
+		clusterGroup.addLayer(m, {
+			maxClusterRadius: 40,
+		});
 	});
-	return L.markerClusterGroup(markers, {
-		maxClusterRadius: 40,
-	});
-};
+	return clusterGroup;
+}
